12f99686092cc271f147478ee1f9183f05ace108
undrfined <undrfined@gmail.com>
2023-02-19 20:30:11 +0200

    test: asdf

--- src/api/makeRequest.ts
+++ src/api/makeRequest.ts
@@ -1,3 +1,3 @@
 export default function makeRequest() {
-  console.log('process.env.APP_API_ENDPOINT', process.env.APP_API_ENDPOINT);
+  console.log('process.env.APP_API_ENDPOINT', `${process.env.APP_API_ENDPOINT}/api`);
 }
