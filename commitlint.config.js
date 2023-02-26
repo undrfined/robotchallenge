0090f58cb0bad19fb35e9120ca62e65624b2dc0c
undrfined <undrfined@gmail.com>
2023-02-26 15:52:16 +0200

    fix: add scopes to commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,19 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-case': [2, 'always', 'kebab-case'],
+        'scope-enum': [
+            2,
+            'always',
+            [
+                'frontend',
+                'backend',
+                'libs',
+                'csharp',
+                'rust',
+                'core',
+                'nginx'
+            ]
+        ]
+    }
+};
